69f547ea7b8e137f316d85bcb5a95a4a02319643
biletweb-macmini <bilet.igor@gmail.com>
2024-12-23 21:57:35 +0200

    отцентрирована форма регистрации

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,70 +1,72 @@
 <template>
-  <div class="card mx-auto w-6/12 rounded-lg border bg-white p-4">
-    <h1 class="text-3xl font-bold">Register</h1>
-    <form @submit.prevent="register">
-      <div class="my-4">
-        <label for="name">Name<span class="ms-1 text-red-500">*</span></label>
-        <input
-          v-model="data.user.name"
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Name"
-          class="w-full rounded-lg border p-2 focus:border-2 focus:border-blue-500 focus:outline-none"
-        />
-      </div>
-
-      <div class="my-4">
-        <label for="email">Email<span class="ms-1 text-red-500">*</span></label>
-        <input
-          v-model="data.user.email"
-          type="text"
-          name="email"
-          id="email"
-          placeholder="Email"
-          class="w-full rounded-lg border p-2 focus:border-2 focus:border-blue-500 focus:outline-none"
-        />
-      </div>
-
-      <div class="my-4">
-        <label for="password">Password<span class="ms-1 text-red-500">*</span></label>
-        <input
-          v-model="data.user.password"
-          type="password"
-          name="password"
-          id="password"
-          placeholder="Password"
-          class="w-full rounded-lg border p-2 focus:border-2 focus:border-blue-500 focus:outline-none"
-        />
-      </div>
-
-      <div class="my-4">
-        <label for="password_confirmation"
-          >Password confirmation<span class="ms-1 text-red-500">*</span></label
-        >
-        <input
-          v-model="data.user.password_confirmation"
-          type="password"
-          name="password_confirmation"
-          id="password_confirmation"
-          placeholder="Password confirmation"
-          class="w-full rounded-lg border p-2 focus:border-2 focus:border-blue-500 focus:outline-none"
-        />
-      </div>
-
-      <div class="my-4">
-        <div v-if="data.loading" class="flex justify-center">
-          <Spinner class="w-10" />
+  <div class="min-h-[calc(100vh-160px)] flex items-center justify-center">
+    <div class="card mx-auto w-6/12 rounded-lg border bg-white p-4">
+      <h1 class="text-3xl font-bold">Register</h1>
+      <form @submit.prevent="register">
+        <div class="my-4">
+          <label for="name">Name<span class="ms-1 text-red-500">*</span></label>
+          <input
+            v-model="data.user.name"
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Name"
+            class="w-full rounded-lg border p-2 focus:border-2 focus:border-blue-500 focus:outline-none"
+          />
         </div>
-        <button
-          v-else
-          type="submit"
-          class="w-full rounded-lg bg-blue-500 px-4 py-2 text-white transition duration-300 hover:bg-blue-600"
-        >
-          Register
-        </button>
-      </div>
-    </form>
+
+        <div class="my-4">
+          <label for="email">Email<span class="ms-1 text-red-500">*</span></label>
+          <input
+            v-model="data.user.email"
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Email"
+            class="w-full rounded-lg border p-2 focus:border-2 focus:border-blue-500 focus:outline-none"
+          />
+        </div>
+
+        <div class="my-4">
+          <label for="password">Password<span class="ms-1 text-red-500">*</span></label>
+          <input
+            v-model="data.user.password"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            class="w-full rounded-lg border p-2 focus:border-2 focus:border-blue-500 focus:outline-none"
+          />
+        </div>
+
+        <div class="my-4">
+          <label for="password_confirmation"
+            >Password confirmation<span class="ms-1 text-red-500">*</span></label
+          >
+          <input
+            v-model="data.user.password_confirmation"
+            type="password"
+            name="password_confirmation"
+            id="password_confirmation"
+            placeholder="Password confirmation"
+            class="w-full rounded-lg border p-2 focus:border-2 focus:border-blue-500 focus:outline-none"
+          />
+        </div>
+
+        <div class="my-4">
+          <div v-if="data.loading" class="flex justify-center">
+            <Spinner class="w-10" />
+          </div>
+          <button
+            v-else
+            type="submit"
+            class="w-full rounded-lg bg-blue-500 px-4 py-2 text-white transition duration-300 hover:bg-blue-600"
+          >
+            Register
+          </button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
