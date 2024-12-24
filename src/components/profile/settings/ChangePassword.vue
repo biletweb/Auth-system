d2fb4c9357d8ea8da0271306f2e8407f6e18cf37
biletweb-macmini <bilet.igor@gmail.com>
2024-12-24 18:34:38 +0200

    fix

---
+++ src/components/profile/settings/ChangePassword.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="card rounded-lg bg-white p-4">
+    <span class="text-xl font-bold">New password</span>
+    <form @submit.prevent="changePassword">
+      <div class="grid grid-cols-2 gap-4">
+        <div class="relative my-4">
+          <label for="password">Password<sup class="ms-1 text-red-500">*</sup></label>
+          <div class="absolute left-2.5 top-[34px] text-gray-400">
+            <i class="pi pi-key"></i>
+          </div>
+          <input
+            v-model="data.user.password"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            class="w-full rounded-lg border p-2 pl-8 focus:border-blue-500 focus:outline-none"
+          />
+        </div>
+        <div class="relative my-4">
+          <label for="password_confirmation">
+            Password confirmation<sup class="ms-1 text-red-500">*</sup>
+          </label>
+          <div class="absolute left-2.5 top-[34px] text-gray-400">
+            <i class="pi pi-key"></i>
+          </div>
+          <input
+            v-model="data.user.password_confirmation"
+            type="password"
+            name="password_confirmation"
+            id="password_confirmation"
+            placeholder="Password confirmation"
+            class="w-full rounded-lg border p-2 pl-8 focus:border-blue-500 focus:outline-none"
+          />
+        </div>
+      </div>
+      <div class="flex justify-end">
+        <button
+          type="submit"
+          class="rounded-lg bg-blue-500 px-4 py-2 text-white transition duration-300 hover:bg-blue-600"
+        >
+          <Spinner v-if="data.loading" class="w-6" />
+          <span v-else>Change password</span>
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { BASE_URL, getConfig } from '@/helpers/config.js'
+import { useAuthStore } from '@/stores/authStore.js'
+import { useToast } from 'vue-toastification'
+import { useRouter } from 'vue-router'
+import { reactive } from 'vue'
+import Spinner from '@/components/Spinner.vue'
+
+const toast = useToast()
+const router = useRouter()
+const authStore = useAuthStore()
+
+const data = reactive({
+  loading: false,
+  user: {
+    password: '',
+    password_confirmation: '',
+  },
+})
+
+const changePassword = async () => {
+  data.loading = true
+  try {
+    const response = await axios.post(
+      `${BASE_URL}/profile/settings/change-password`,
+      data.user,
+      getConfig(authStore.access_token),
+    )
+    if (response.data.error) {
+      toast.error(response.data.error, { timeout: 5000 })
+    } else {
+      toast.success(response.data.message, { timeout: 5000 })
+      data.user.password = ''
+      data.user.password_confirmation = ''
+    }
+  } catch (error) {
+    const errors = error.response.data.errors
+    let errorMessage = ''
+    errorMessage = Object.values(errors).flat().join('\n')
+    toast.error(errorMessage, { timeout: 5000 })
+    if (error.response.status === 401) {
+      authStore.clearState()
+      router.push({ name: 'login' })
+      toast.error(error.response.data.message, { timeout: 5000 })
+    }
+  } finally {
+    data.loading = false
+  }
+}
+</script>
