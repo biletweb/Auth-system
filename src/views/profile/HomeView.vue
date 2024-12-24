b360b8b1c7b4eb44d0b5edbe86ea2e741ad9f9a1
biletweb-macmini <bilet.igor@gmail.com>
2024-12-24 02:01:20 +0200

    добавлена кнопка обновления профиля

--- src/views/profile/HomeView.vue
+++ src/views/profile/HomeView.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="mb-4 flex items-center justify-between">
-    <div class="text-3xl font-bold">Profile</div>
+    <div>
+      <span class="text-3xl font-bold">Profile</span>
+      <button
+        @click="syncProfile"
+        class="text-blue-500 transition duration-300 hover:text-blue-600"
+      >
+        <i class="pi pi-sync ms-2" style="font-size: 25px"></i>
+      </button>
+    </div>
     <div>
       <router-link
         :to="{ name: 'admin' }"
@@ -11,20 +19,44 @@
     </div>
   </div>
   <div class="card rounded-lg bg-white p-4">
-    <div v-if="!authStore.user" class="flex justify-center">
+    <div v-if="!authStore.user || loading" class="flex justify-center">
       <Spinner class="w-10 rounded-full bg-blue-500 p-1" />
     </div>
-    <span v-else class="text-xl"
-      >Hello <span class="font-bold">{{ authStore.user?.name }}</span
+    <span v-else class="text-xl">
+      Hello <span class="font-bold">{{ authStore.user?.name }}</span
       >, you are logged in. Email to contact you:
-      <span class="font-bold">{{ authStore.user?.email }}</span></span
-    >
+      <span class="font-bold">{{ authStore.user?.email }}</span>
+    </span>
   </div>
 </template>
 
 <script setup>
 import { useAuthStore } from '@/stores/authStore.js'
 import Spinner from '@/components/Spinner.vue'
+import axios from 'axios'
+import { BASE_URL, getConfig } from '@/helpers/config.js'
+import { useToast } from 'vue-toastification'
+import { useRouter } from 'vue-router'
+import { ref } from 'vue'
 
+const toast = useToast()
+const router = useRouter()
 const authStore = useAuthStore()
+const loading = ref(false)
+
+const syncProfile = async () => {
+  loading.value = true
+  try {
+    const response = await axios.get(`${BASE_URL}/profile`, getConfig(authStore.access_token))
+    authStore.setUser(response.data.user)
+  } catch (error) {
+    if (error.response.status === 401) {
+      authStore.clearState()
+      router.push({ name: 'login' })
+      toast.error(error.response.data.message, { timeout: 5000 })
+    }
+  } finally {
+    loading.value = false
+  }
+}
 </script>
