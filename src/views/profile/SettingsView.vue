1ca0a455d328d80e54d116fa8fc742d50201acf2
biletweb-macmini <bilet.igor@gmail.com>
2024-12-25 18:50:04 +0200

    fix

--- src/views/profile/SettingsView.vue
+++ src/views/profile/SettingsView.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="mb-4">
+  <div class="mb-4 flex items-center justify-between">
     <span class="text-3xl font-bold">Settings</span>
+    <Spinner v-if="!authStore.user" class="w-7 rounded-full bg-blue-500 p-1" />
+    <span v-else class="text-xl font-bold">{{ authStore.user.email }}</span>
   </div>
   <div class="grid grid-cols-2 gap-4">
     <ChangePassword />
@@ -15,4 +17,8 @@
 import ChangePassword from '@/components/profile/settings/ChangePassword.vue'
 import UpdatePersonalInfo from '@/components/profile/settings/UpdatePersonalInfo.vue'
 import DeleteAccount from '@/components/profile/settings/DeleteAccount.vue'
+import Spinner from '@/components/Spinner.vue'
+import { useAuthStore } from '@/stores/authStore.js'
+
+const authStore = useAuthStore()
 </script>
