deeec86a94e8b307406f4c58cc398c41054a7672
biletweb-macmini <bilet.igor@gmail.com>
2024-12-25 23:58:22 +0200

    fix

--- src/views/profile/SettingsView.vue
+++ src/views/profile/SettingsView.vue
@@ -16,24 +16,26 @@
         </button>
       </div>
     </div>
-    <div class="mt-4 flex w-52 gap-4">
-      <div>
-        <input
-          type="text"
-          name="code"
-          id="code"
-          placeholder="Code"
-          class="w-full rounded-lg border p-2 font-normal focus:border-blue-500 focus:outline-none"
-        />
+    <form @submit.prevent="confirmEmail">
+      <div class="mt-4 flex w-60 gap-4">
+        <div>
+          <input
+            v-model="securityCode"
+            type="text"
+            name="security_code"
+            placeholder="Security code"
+            class="w-full rounded-lg border p-2 font-normal focus:border-blue-500 focus:outline-none"
+          />
+        </div>
+        <div class="align-bottom">
+          <button
+            class="rounded-lg bg-blue-500 px-4 py-2 font-normal text-white transition duration-300 hover:bg-blue-600"
+          >
+            Confirm
+          </button>
+        </div>
       </div>
-      <div class="align-bottom">
-        <button
-          class="rounded-lg bg-blue-500 px-4 py-2 font-normal text-white transition duration-300 hover:bg-blue-600"
-        >
-          Confirm
-        </button>
-      </div>
-    </div>
+    </form>
   </div>
   <div class="grid grid-cols-2 gap-4">
     <ChangePassword />
@@ -51,9 +53,17 @@ import DeleteAccount from '@/components/profile/settings/DeleteAccount.vue'
 import Spinner from '@/components/Spinner.vue'
 import { useAuthStore } from '@/stores/authStore.js'
 import { watch, ref } from 'vue'
+import axios from 'axios'
+import { BASE_URL, getConfig } from '@/helpers/config.js'
+import { useToast } from 'vue-toastification'
+import { useRouter } from 'vue-router'
 
 const authStore = useAuthStore()
 const isEmailVerified = ref(true)
+const securityCode = ref('')
+const loading = ref(false)
+const toast = useToast()
+const router = useRouter()
 
 watch(
   () => authStore.user,
@@ -64,4 +74,35 @@ watch(
   },
   { immediate: true },
 )
+
+const confirmEmail = async () => {
+  loading.value = true
+  try {
+    const response = await axios.post(
+      `${BASE_URL}/profile/settings/confirm-email`,
+      securityCode.value,
+      getConfig(authStore.access_token),
+    )
+    if (response.data.error) {
+      toast.error(response.data.error, { timeout: 5000 })
+    }
+    if (response.data.warning) {
+      toast.warning(response.data.warning, { timeout: 5000 })
+    } else {
+      toast.success(response.data.message, { timeout: 5000 })
+    }
+  } catch (error) {
+    const errors = error.response.data.errors
+    let errorMessage = ''
+    errorMessage = Object.values(errors).flat().join('\n')
+    toast.error(errorMessage, { timeout: 5000 })
+    if (error.response.status === 401) {
+      authStore.clearState()
+      router.push({ name: 'login' })
+      toast.error(error.response.data.message, { timeout: 5000 })
+    }
+  } finally {
+    loading.value = false
+  }
+}
 </script>
