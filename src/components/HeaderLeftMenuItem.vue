6fe9b7dc1fd42281b48774bbdccd517cc0bb85db
biletweb-macmini <bilet.igor@gmail.com>
2024-12-26 16:51:59 +0200

    установлен пакет i18n

--- src/components/HeaderLeftMenuItem.vue
+++ src/components/HeaderLeftMenuItem.vue
@@ -2,7 +2,7 @@
   <ul class="flex items-center gap-4">
     <li class="transition duration-300 hover:text-gray-300">
       <router-link :to="{ name: 'home' }" class="text-lg">
-        <i class="pi pi-home me-1"></i>Home
+        <i class="pi pi-home me-1"></i>{{ $t('Home') }}
       </router-link>
     </li>
     <li class="transition duration-300 hover:text-gray-300">
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import './assets/main.css'
 import 'primeicons/primeicons.css'
 
-import { createApp } from 'vue'
+import { createApp, ref } from 'vue'
+import { createI18n } from 'vue-i18n'
 import { createPinia } from 'pinia'
 import 'vue-toastification/dist/index.css'
 import Toast from 'vue-toastification'
@@ -9,10 +10,28 @@ import Toast from 'vue-toastification'
 import App from './App.vue'
 import router from './router'
 
+import en from './locales/en.json'
+import ru from './locales/ru.json'
+import uk from './locales/uk.json'
+
+const savedLocale = ref('ru')
+
+if (localStorage.getItem('locale')) {
+  savedLocale.value = localStorage.getItem('locale')
+}
+
+const i18n = createI18n({
+  locale: savedLocale.value,
+  fallbackLocale: 'en',
+  messages: { en, ru, uk },
+  globalInjection: true,
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(Toast)
+app.use(i18n)
 
 app.mount('#app')
