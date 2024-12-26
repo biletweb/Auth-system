ee67dad8a382c680dfd70e28e833056cd3de9360
biletweb-macmini <bilet.igor@gmail.com>
2024-12-26 13:30:58 +0200

    fix

--- src/views/profile/SettingsView.vue
+++ src/views/profile/SettingsView.vue
@@ -31,6 +31,7 @@
         </div>
         <div class="align-bottom">
           <button
+            type="submit"
             class="rounded-lg bg-blue-500 px-4 py-2 font-normal text-white transition duration-300 hover:bg-blue-600"
             :disabled="loading"
           >
