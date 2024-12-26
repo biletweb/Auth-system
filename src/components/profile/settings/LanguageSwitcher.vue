cc16d4442fb257dcae12f3d30c447dc47d6c9a73
biletweb-macmini <bilet.igor@gmail.com>
2024-12-26 18:16:14 +0200

    fix

--- src/components/profile/settings/LanguageSwitcher.vue
+++ src/components/profile/settings/LanguageSwitcher.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="card rounded-lg bg-white p-4">
-    <span class="text-xl font-bold">{{ $t('Changing your language') }}</span>
-
+    <div class="flex items-center justify-between">
+      <span class="text-xl font-bold">{{ $t('Changing your language') }}</span>
+      <Spinner v-if="loading" class="w-7 rounded-full bg-blue-500 p-1" />
+    </div>
     <div class="my-7 flex gap-2">
       <button type="submit" @click="setLocale('uk')" :disabled="savedLocale === 'uk'">
         <span
@@ -34,17 +36,53 @@
 <script setup>
 import { ref, onMounted, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
+import axios from 'axios'
+import { BASE_URL, getConfig } from '@/helpers/config.js'
+import { useAuthStore } from '@/stores/authStore.js'
+import { useToast } from 'vue-toastification'
+import { useRouter } from 'vue-router'
+import Spinner from '@/components/Spinner.vue'
+
+const toast = useToast()
+const router = useRouter()
+const authStore = useAuthStore()
 
 const { locale } = useI18n()
 const savedLocale = ref('')
+const loading = ref(false)
 
 onMounted(() => {
   savedLocale.value = localStorage.getItem('locale')
 })
 
-const setLocale = (newLocale) => {
+const setLocale = async (newLocale) => {
   locale.value = newLocale
   localStorage.setItem('locale', newLocale)
+  loading.value = true
+  try {
+    const response = await axios.post(
+      `${BASE_URL}/profile/settings/change-locale`,
+      { locale: newLocale },
+      getConfig(authStore.access_token),
+    )
+    if (response.data.error) {
+      toast.error(response.data.error, { timeout: 5000 })
+    } else {
+      toast.success(response.data.message, { timeout: 5000 })
+    }
+  } catch (error) {
+    const errors = error.response.data.errors
+    let errorMessage = ''
+    errorMessage = Object.values(errors).flat().join('\n')
+    toast.error(errorMessage, { timeout: 5000 })
+    if (error.response.status === 401) {
+      authStore.clearState()
+      router.push({ name: 'login' })
+      toast.error(error.response.data.message, { timeout: 5000 })
+    }
+  } finally {
+    loading.value = false
+  }
 }
 
 watch(locale, (newValue) => {
