a6cdc943ea27486de009666c9f02033eb6d60a31
biletweb-macmini <bilet.igor@gmail.com>
2024-12-29 17:17:47 +0200

    fix

--- src/views/admin/users/UsersView.vue
+++ src/views/admin/users/UsersView.vue
@@ -59,6 +59,13 @@
               >
                 <div class="p-2">
                   <button
+                    @click="sortBy('all')"
+                    type="button"
+                    class="flex w-full cursor-pointer items-center rounded-lg px-4 py-2 text-sm font-normal text-slate-500 hover:bg-gray-50 hover:text-slate-600"
+                  >
+                    {{ $t('All') }}
+                  </button>
+                  <button
                     @click="sortBy('admin')"
                     type="button"
                     class="flex w-full cursor-pointer items-center rounded-lg px-4 py-2 text-sm font-normal text-red-500 hover:bg-gray-50 hover:text-red-600"
