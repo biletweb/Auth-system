d9aabd3371914504b9834fd7b2125a8481f84be3
biletweb-macmini <bilet.igor@gmail.com>
2024-12-31 00:28:36 +0200

    fix

--- src/views/auth/ForgotPasswordView.vue
+++ src/views/auth/ForgotPasswordView.vue
@@ -2,7 +2,7 @@
   <div class="flex min-h-[calc(100vh-164px)] items-center justify-center">
     <div class="card sm:w-12/12 mx-auto rounded-lg border bg-white p-4 shadow lg:w-6/12">
       <span class="text-3xl font-bold">{{ $t('Forgot password?') }}</span>
-      <form @submit.prevent="">
+      <form @submit.prevent="sendNewPassword">
         <div class="relative my-4">
           <label for="email">{{ $t('Email') }}<sup class="ms-1 text-red-500">*</sup></label>
           <div class="absolute left-2.5 top-[34px] text-gray-400"><i class="pi pi-at"></i></div>
@@ -35,8 +35,36 @@
 <script setup>
 import { ref } from 'vue'
 import Spinner from '@/components/Spinner.vue'
+import axios from 'axios'
+import { BASE_URL } from '@/helpers/config.js'
+import { useRouter } from 'vue-router'
+import { useToast } from 'vue-toastification'
+import { i18n } from '@/main.js'
 
+const toast = useToast()
+const router = useRouter()
 const errorField = ref('')
 const loading = ref(false)
 const email = ref('')
+
+const sendNewPassword = async () => {
+  loading.value = true
+  errorField.value = ''
+  try {
+    const response = await axios.post(`${BASE_URL}/login/forgot-password`, { email })
+    if (response.data.error) {
+      toast.error(i18n.global.t(response.data.error), { timeout: 5000, pauseOnFocusLoss: true })
+    } else {
+      router.push({ name: 'login' })
+      toast.success(i18n.global.t(response.data.message), { timeout: 5000, pauseOnFocusLoss: true })
+    }
+  } catch (error) {
+    if (error.response.status === 422) {
+      errorField.value = error.response.data.field
+      toast.error(i18n.global.t(error.response.data.error), { timeout: 5000, pauseOnFocusLoss: true })
+    }
+  } finally {
+    loading.value = false
+  }
+}
 </script>
