515ac139676ea9c664445d6d63e823df317063a8
biletweb-macmini <bilet.igor@gmail.com>
2024-12-31 19:26:11 +0200

    fix

--- src/views/profile/SettingsView.vue
+++ src/views/profile/SettingsView.vue
@@ -33,8 +33,11 @@
             type="text"
             name="verificationCode"
             :placeholder="$t('Verification code')"
-            class="w-44 rounded-lg border p-2 font-normal focus:border-blue-500 focus:outline-none"
-            :class="{ 'border-red-500': errorField === 'verification_code' }"
+            class="w-44 rounded-lg border p-2 font-normal focus:outline-none"
+            :class="{
+              'focus:border-blue-500': errorField === '',
+              'border-red-500': errorField === 'verification_code',
+            }"
           />
         </div>
         <div class="align-bottom">
