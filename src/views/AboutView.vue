8dbc2f05a0256a8c05291f579cfae8970bdfed55
biletweb-macmini <bilet.igor@gmail.com>
2025-01-01 18:47:32 +0200

    fix

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="mb-4">
-    <span class="text-3xl font-bold dark:text-red-500">{{ $t('About') }}</span>
+    <span class="text-3xl font-bold text-slate-900 dark:text-white">{{ $t('About') }}</span>
+    <p class="mt-4 text-slate-500 dark:text-slate-400">Using Tailwind CSS to style your site in dark mode.</p>
   </div>
 </template>
