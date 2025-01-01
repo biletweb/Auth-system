66bda8476300f7dc8316c9c21a4552dcfdc34711
biletweb-macmini <bilet.igor@gmail.com>
2025-01-02 01:23:05 +0200

    fix

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,44 +1,50 @@
 <template>
   <div class="flex min-h-[calc(100vh-164px)] items-center justify-center">
-    <div class="card sm:w-12/12 mx-auto rounded-lg border bg-white p-4 shadow lg:w-6/12">
-      <span class="text-3xl font-bold">{{ $t('Login') }}</span>
+    <div class="card sm:w-12/12 mx-auto rounded-lg border bg-white p-4 shadow lg:w-6/12 dark:border-slate-600 dark:bg-slate-800">
+      <span class="text-3xl font-bold dark:text-white">{{ $t('Login') }}</span>
       <form @submit.prevent="login">
         <div class="relative my-4">
-          <label for="email">{{ $t('Email') }}<sup class="ms-1 text-red-500">*</sup></label>
-          <div class="absolute left-2.5 top-[34px] text-gray-400"><i class="pi pi-at"></i></div>
+          <label for="email" class="dark:text-slate-200">
+            {{ $t('Email') }}<sup class="ms-1 text-red-500 dark:text-rose-500">*</sup>
+          </label>
+          <div class="absolute left-2.5 top-[34px] text-slate-400"><i class="pi pi-at"></i></div>
           <input
             v-model="data.user.email"
             type="text"
             name="email"
             id="email"
             :placeholder="$t('Email')"
-            class="w-full rounded-lg border p-2 pl-8 focus:outline-none"
+            class="w-full rounded-lg border p-2 pl-8 placeholder:text-slate-400 focus:outline-none dark:bg-slate-800 dark:text-slate-400"
             :class="{
-              'focus:border-blue-500': errorField === '',
-              'border-red-500': errorField === 'email' || errorField === 'email, password',
+              'focus:border-blue-500 dark:border-slate-600 dark:focus:border-indigo-500':
+                errorField !== 'email' || errorField !== 'email, password',
+              'border-red-500 dark:border-rose-500': errorField === 'email' || errorField === 'email, password',
             }"
           />
         </div>
         <div class="relative my-4">
-          <label for="password">{{ $t('Password') }}<sup class="ms-1 text-red-500">*</sup></label>
-          <div class="absolute left-2.5 top-[34px] text-gray-400"><i class="pi pi-key"></i></div>
+          <label for="password" class="dark:text-slate-200">
+            {{ $t('Password') }}<sup class="ms-1 text-red-500 dark:text-rose-500">*</sup>
+          </label>
+          <div class="absolute left-2.5 top-[34px] text-slate-400"><i class="pi pi-key"></i></div>
           <input
             v-model="data.user.password"
             type="password"
             name="password"
             id="password"
             :placeholder="$t('Password')"
-            class="w-full rounded-lg border p-2 pl-8 focus:outline-none"
+            class="w-full rounded-lg border p-2 pl-8 placeholder:text-slate-400 focus:outline-none dark:bg-slate-800 dark:text-slate-400"
             :class="{
-              'focus:border-blue-500': errorField === '',
-              'border-red-500': errorField === 'password' || errorField === 'email, password',
+              'focus:border-blue-500 dark:border-slate-600 dark:focus:border-indigo-500':
+                errorField !== 'password' || errorField !== 'email, password',
+              'border-red-500 dark:border-rose-500': errorField === 'password' || errorField === 'email, password',
             }"
           />
         </div>
         <div class="my-4 flex justify-end">
           <router-link
             :to="{ name: 'login.forgot-password' }"
-            class="font-semibold text-blue-500 transition duration-300 hover:text-blue-600 hover:underline hover:underline-offset-4"
+            class="font-semibold text-blue-500 transition duration-300 hover:text-blue-600 hover:underline hover:underline-offset-4 dark:text-indigo-500 dark:hover:text-indigo-400"
           >
             {{ $t('Forgot password?') }}
           </router-link>
@@ -46,7 +52,7 @@
         <div class="my-4">
           <button
             type="submit"
-            class="flex w-full justify-center rounded-lg bg-blue-500 px-4 py-2 text-white transition duration-300 hover:bg-blue-600"
+            class="flex w-full justify-center rounded-lg bg-blue-500 px-4 py-2 text-white transition duration-300 hover:bg-blue-600 dark:bg-indigo-500 dark:hover:bg-indigo-400"
             :disabled="data.loading"
           >
             <Spinner v-if="data.loading" class="w-6" />
