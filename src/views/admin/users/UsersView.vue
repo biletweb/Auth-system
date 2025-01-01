a4be51d43cf620d34f042f6e23348edccc8ece08
biletweb-macmini <bilet.igor@gmail.com>
2025-01-01 20:43:09 +0200

    fix

--- src/views/admin/users/UsersView.vue
+++ src/views/admin/users/UsersView.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="mb-4 flex items-center justify-between">
     <div>
-      <span class="text-3xl font-bold">{{ $t('Users') }}</span>
+      <span class="text-3xl font-bold dark:text-white">{{ $t('Users') }}</span>
     </div>
     <div class="flex items-center">
       <router-link
         :to="{ name: 'admin' }"
-        class="flex items-center font-bold text-blue-500 transition duration-300 hover:text-blue-600"
+        class="flex items-center font-bold text-blue-500 transition duration-300 hover:text-blue-600 dark:text-indigo-500 dark:hover:text-indigo-400"
       >
         <i class="pi pi-step-backward me-1"></i>{{ $t('Menu') }}
       </router-link>
