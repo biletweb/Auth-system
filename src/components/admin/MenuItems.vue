f8c507246adde591dbd1e150da0794c759be25e4
biletweb-macmini <bilet.igor@gmail.com>
2025-01-01 20:24:27 +0200

    fix

--- src/components/admin/MenuItems.vue
+++ src/components/admin/MenuItems.vue
@@ -1,7 +1,7 @@
 <template>
   <router-link
     :to="{ name: 'admin.users' }"
-    class="flex items-center font-bold text-blue-500 transition duration-300 hover:text-blue-600"
+    class="flex items-center font-semibold text-blue-500 transition duration-300 hover:text-blue-600"
   >
     <i class="pi pi-users me-1"></i>{{ $t('Users') }}
   </router-link>
--- src/components/profile/MenuItems.vue
+++ src/components/profile/MenuItems.vue
@@ -2,7 +2,7 @@
   <div v-if="authStore.user?.role === 'admin'">
     <router-link
       :to="{ name: 'admin' }"
-      class="flex items-center font-bold text-red-500 transition duration-300 hover:text-red-600"
+      class="flex items-center font-semibold text-red-500 transition duration-300 hover:text-red-600"
     >
       <i class="pi pi-wrench me-1"></i>{{ $t('Administration panel') }}
     </router-link>
@@ -10,7 +10,7 @@
   <div>
     <router-link
       :to="{ name: 'profile.settings' }"
-      class="flex items-center font-bold text-blue-500 transition duration-300 hover:text-blue-600"
+      class="flex items-center font-semibold text-blue-500 transition duration-300 hover:text-blue-600"
     >
       <i class="pi pi-cog me-1"></i>{{ $t('Settings') }}
     </router-link>
