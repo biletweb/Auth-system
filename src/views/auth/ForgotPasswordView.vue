18800f94671f212a38a7b20688935cc12c487bdd
biletweb-macmini <bilet.igor@gmail.com>
2025-01-02 01:34:25 +0200

    fix

--- src/views/auth/ForgotPasswordView.vue
+++ src/views/auth/ForgotPasswordView.vue
@@ -1,29 +1,31 @@
 <template>
   <div class="flex min-h-[calc(100vh-164px)] items-center justify-center">
-    <div class="card sm:w-12/12 mx-auto rounded-lg border bg-white p-4 shadow lg:w-6/12">
-      <span class="text-3xl font-bold">{{ $t('Forgot password?') }}</span>
+    <div class="card sm:w-12/12 mx-auto rounded-lg border bg-white p-4 shadow lg:w-6/12 dark:border-slate-600 dark:bg-slate-800">
+      <span class="text-3xl font-bold dark:text-white">{{ $t('Forgot password?') }}</span>
       <form @submit.prevent="sendNewPassword">
         <div class="relative my-4">
-          <label for="email">{{ $t('Email') }}<sup class="ms-1 text-red-500">*</sup></label>
-          <div class="absolute left-2.5 top-[34px] text-gray-400"><i class="pi pi-at"></i></div>
+          <label for="email" class="dark:text-slate-200">
+            {{ $t('Email') }}<sup class="ms-1 text-red-500 dark:text-rose-500">*</sup>
+          </label>
+          <div class="absolute left-2.5 top-[34px] text-slate-400"><i class="pi pi-at"></i></div>
           <input
             v-model="email"
             type="text"
             name="email"
             id="email"
             :placeholder="$t('Email')"
-            class="w-full rounded-lg border p-2 pl-8 focus:outline-none"
+            class="w-full rounded-lg border p-2 pl-8 placeholder:text-slate-400 focus:outline-none dark:bg-slate-800 dark:text-slate-400"
             :class="{
-              'focus:border-blue-500': errorField === '',
-              'border-red-500': errorField === 'email',
+              'focus:border-blue-500 dark:border-slate-600 dark:focus:border-indigo-500': errorField !== 'email',
+              'border-red-500 dark:border-rose-500': errorField === 'email',
             }"
           />
-          <p class="text-xs text-slate-500">{{ $t('Enter the email address you used when registering.') }}</p>
+          <p class="text-xs text-slate-500 dark:text-slate-400">{{ $t('Enter the email address you used when registering.') }}</p>
         </div>
         <div class="my-4">
           <button
             type="submit"
-            class="flex w-full justify-center rounded-lg bg-blue-500 px-4 py-2 text-white transition duration-300 hover:bg-blue-600"
+            class="flex w-full justify-center rounded-lg bg-blue-500 px-4 py-2 text-white transition duration-300 hover:bg-blue-600 dark:bg-indigo-500 dark:hover:bg-indigo-400"
             :disabled="loading"
           >
             <Spinner v-if="loading" class="w-6" />
