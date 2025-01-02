459f69b3f632d72456e965d0c612fcbca3ea1db6
biletweb-macmini <bilet.igor@gmail.com>
2025-01-02 18:05:55 +0200

    fix

--- src/components/HeaderLogo.vue
+++ src/components/HeaderLogo.vue
@@ -1,3 +1,3 @@
 <template>
-  <router-link :to="{ name: 'home' }"><img src="/logo.webp" alt="Logo" class="size-10" /></router-link>
+  <router-link :to="{ name: 'home' }"><img src="/logo.png" alt="Logo" class="size-10" /></router-link>
 </template>
