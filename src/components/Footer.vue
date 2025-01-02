253fb6c224675e3cd1b7f5332c2e6f8e96e53fee
biletweb-macmini <bilet.igor@gmail.com>
2025-01-02 19:07:50 +0200

    fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,14 @@
 <template>
   <footer class="rounded-t-lg bg-slate-500 py-4 text-white dark:bg-slate-800 dark:text-slate-400">
-    <div class="text-center text-lg">© {{ currentYear }}, {{ $t('All rights reserved') }}</div>
+    <div class="text-center text-lg">
+      {{ $t('Copyright') }} © 2024-{{ currentYear }} {{ $t('All rights reserved') }}. {{ $t('Version:') }} {{ appVersion }}
+    </div>
   </footer>
 </template>
 
 <script setup>
+import version from '../../package.json'
 const currentYear = new Date().getFullYear()
+
+const appVersion = version.version
 </script>
