dd81b5ced9a758c149cedfbc37a7f6c0358eb331
biletweb-macmini <bilet.igor@gmail.com>
2025-01-02 22:10:48 +0200

    fix

--- src/views/admin/HomeView.vue
+++ src/views/admin/HomeView.vue
@@ -7,7 +7,7 @@
       <MenuItems />
     </div>
   </div>
-  <div class="my-4 grid grid-cols-2 gap-4">
+  <div class="my-4 grid grid-cols-3 gap-4">
     <div class="card rounded-lg border bg-white p-4 dark:border-slate-600 dark:bg-slate-800">
       <div>
         <p class="dark:text-slate-200">{{ $t('Users') }}</p>
@@ -18,6 +18,12 @@
       <div>
         <p class="dark:text-slate-200">{{ $t('Administrators') }}</p>
         <p class="text-2xl font-medium dark:text-cyan-500">7</p>
+      </div>
+    </div>
+    <div class="card rounded-lg border bg-white p-4 dark:border-slate-600 dark:bg-slate-800">
+      <div>
+        <p class="dark:text-slate-200">{{ $t('Unconfirmed email addresses') }}</p>
+        <p class="text-2xl font-medium dark:text-cyan-500">3</p>
       </div>
     </div>
   </div>
